095ddbfa93235339a53fc6f5b3b203591410acdc
Luke Dias <lhollowwizard@gmail.com>
2020-10-23 13:49:01 -0300

    Finished the project, added some stuff. I still need to make it possible to add many musics...

--- MusicPlayer/Form1.Designer.cs
+++ MusicPlayer/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MusicPlayer
 {
-    partial class Form1
+    partial class MusicPlayerForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayerForm));
+            this.Player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Browse = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Pause = new System.Windows.Forms.Button();
+            this.Play = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // Player
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "MusicPlayerForm";
+            this.Player.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Player.Enabled = true;
+            this.Player.Location = new System.Drawing.Point(2, -16);
+            this.Player.Name = "Player";
+            this.Player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("Player.OcxState")));
+            this.Player.Size = new System.Drawing.Size(715, 295);
+            this.Player.TabIndex = 0;
+            // 
+            // Browse
+            // 
+            this.Browse.BackColor = System.Drawing.Color.Gray;
+            this.Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Browse.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Browse.Location = new System.Drawing.Point(4, 285);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(137, 45);
+            this.Browse.TabIndex = 2;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = false;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Gray;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Exit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Exit.Location = new System.Drawing.Point(576, 285);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(137, 45);
+            this.Exit.TabIndex = 3;
+            this.Exit.Text = "Exit";
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.Color.Gray;
+            this.Stop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Stop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Stop.Location = new System.Drawing.Point(433, 285);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(137, 45);
+            this.Stop.TabIndex = 4;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Pause
+            // 
+            this.Pause.BackColor = System.Drawing.Color.Gray;
+            this.Pause.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Pause.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Pause.Location = new System.Drawing.Point(290, 285);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(137, 45);
+            this.Pause.TabIndex = 5;
+            this.Pause.Text = "Pause";
+            this.Pause.UseVisualStyleBackColor = false;
+            this.Pause.Click += new System.EventHandler(this.Pause_Click);
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Gray;
+            this.Play.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Play.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Play.Location = new System.Drawing.Point(147, 285);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(137, 45);
+            this.Play.TabIndex = 6;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 247);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
+            this.textBox1.TabIndex = 7;
+            // 
+            // MusicPlayerForm
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Equation;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(715, 338);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.Pause);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.Player);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MusicPlayerForm";
+            this.Text = "Music Player";
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer Player;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Pause;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- MusicPlayer/Form1.cs
+++ MusicPlayer/Form1.cs
@@ -1,20 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security;
 using System.Windows.Forms;
 
 namespace MusicPlayer
 {
-    public partial class Form1 : Form
+    public partial class MusicPlayerForm : Form
     {
-        public Form1()
+
+
+        public MusicPlayerForm()
         {
             InitializeComponent();
+            var musicDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+            openFileDialog.InitialDirectory = musicDirectory;
+            openFileDialog.Filter = "Music Files(*.mp3;*.wav ;*.wma;*.asf;*.mp4;*.mpg;*.mpa;*.zpl)|*.mp3;*.wav;*.wma;*.asf;*.mp4;*.mpg ;*.mpa;*.zpl";
+            openFileDialog.Multiselect = true;
+            openFileDialog.Title = "Choose a music";
+            openFileDialog.RestoreDirectory = false;
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                foreach(string filePath in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        Player.URL += filePath;
+                    }
+                    catch (SecurityException ex)
+                    {
+                        // The user lacks appropriate permissions to read files, discover paths, etc.
+                        MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                                        "Error message: " + ex.Message + "\n\n" +
+                                        "Details (send to Support):\n\n" + ex.StackTrace);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Could not load the sound - probably related to Windows file system permissions.
+                        MessageBox.Show("Cannot play the sound " + filePath.Substring(filePath.LastIndexOf('\\'))
+                            + ". You may not have permission to read the file, or " +
+                            "it may be corrupt.\n\nReported error: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.play();
+        }
+
+        private void Pause_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.pause();
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            Player.Ctlcontrols.stop();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
